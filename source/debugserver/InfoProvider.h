c21bf4ffaa047b33d72fbef6a37138bb56625cba
Dongsu Jang <77693+iolo@users.noreply.github.com>
2026-01-04 22:55:25 +0900

    fix build error: add include stdint.h

--- source/debugserver/InfoProvider.h
+++ source/debugserver/InfoProvider.h
@@ -30,6 +30,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include <string>
 #include <functional>
+#include <stdint.h>
 #include "HttpRequest.h"
 #include "HttpResponse.h"
 
